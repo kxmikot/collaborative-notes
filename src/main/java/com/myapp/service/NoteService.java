d7e3426be84a3863ab8b4b028c26d07ce95285c1
KXMIKOTO <kxmikoto@gmail.com>
2025-09-24 02:49:59 +0300

    +: Note service methods added

--- src/main/java/com/myapp/service/NoteService.java
+++ src/main/java/com/myapp/service/NoteService.java
@@ -2,11 +2,16 @@ package com.myapp.service;
 
 import com.myapp.dao.NoteDAO;
 import com.myapp.model.Note;
+import java.util.List;
 
 public class NoteService {
     private final NoteDAO noteDAO = new NoteDAO();
 
     public void createNote(int userId, String title, String content) {
         noteDAO.addNote(new Note(userId, title, content));
+    }
+
+    public List<Note> getUserNotes(int userId) {
+        return noteDAO.getNotesByUserId(userId);
     }
 }
