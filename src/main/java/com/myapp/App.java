a41ed8d12a91598846252847f871fbe331fa09ef
KXMIKOTO <kxmikoto@gmail.com>
2025-09-24 02:57:48 +0300

    +: Changes to App.java for creating new user

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -2,8 +2,18 @@ package com.myapp;
 
 import java.sql.*;
 
+import com.myapp.dao.UserDAO;
+import com.myapp.service.AuthService;
+
 public class App {
     public static void main(String[] args) {
-        
+        new UserDAO().createTable();
+
+        AuthService authService = new AuthService();
+        boolean registered = authService.register("user1", "pass123");
+        System.out.println("Registered " + registered);
+
+        boolean loggedIn = authService.login("user1", "pass123");
+        System.out.println("Logged in: " + loggedIn);
     }
 }
