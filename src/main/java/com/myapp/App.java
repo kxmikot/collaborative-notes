99f904df67f3c89608ed9356f440aca82de7dbb7
KXMIKOTO <kxmikoto@gmail.com>
2025-09-24 03:28:49 +0300

    +: getUserNotes testing in App.java

--- src/main/java/com/myapp/App.java
+++ src/main/java/com/myapp/App.java
@@ -2,12 +2,17 @@ package com.myapp;
 
 import java.sql.*;
 
+import com.myapp.dao.NoteDAO;
 import com.myapp.dao.UserDAO;
 import com.myapp.service.AuthService;
+import com.myapp.service.NoteService;
+import java.util.List;
+import com.myapp.model.*;
 
 public class App {
     public static void main(String[] args) {
         new UserDAO().createTable();
+        new NoteDAO().createTable();
 
         AuthService authService = new AuthService();
         boolean registered = authService.register("user1", "pass123");
@@ -15,5 +20,14 @@ public class App {
 
         boolean loggedIn = authService.login("user1", "pass123");
         System.out.println("Logged in: " + loggedIn);
+
+        NoteService noteService = new NoteService();
+        //noteService.createNote(0, "Java OCA", "Certification");
+
+        List<Note> notes = noteService.getUserNotes(0);
+        for (Note n : notes) {
+            System.out.println(n.getTitle());
+            System.out.println(n.getContent());
+        }
     }
 }
